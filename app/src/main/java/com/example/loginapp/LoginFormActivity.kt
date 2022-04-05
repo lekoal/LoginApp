99a9681931a51d5a1c37737274d1db302f0dd68a
lekoal <coreglus@gmail.com>
2022-04-05 16:29:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/loginapp/LoginFormActivity.kt
@@ -0,0 +1,12 @@
+package com.example.loginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.loginapp.R
+
+class LoginFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_form)
+    }
+}
