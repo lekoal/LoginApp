d6455d9f3861052000e3d75a03f8e5066a71bc76
andlekoal <andlekoal@gmail.com>
2022-07-03 18:53:58 +0300

    Mockito implementation added

---
+++ app/src/test/java/com/example/loginapp/LoginFormPresenterTest.kt
@@ -0,0 +1,9 @@
+package com.example.loginapp
+
+import com.example.loginapp.ui.login.LoginFormPresenter
+
+class LoginFormPresenterTest {
+    private lateinit var presenter: LoginFormPresenter
+
+
+}
